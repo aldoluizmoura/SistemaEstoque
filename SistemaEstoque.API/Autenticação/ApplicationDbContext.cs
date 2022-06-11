949114006a33239ab9a2c072edb3ccb27638430a
Aldo Luiz Moura Silva Moreira <aldo.moreira@agilecontent.com>
2022-06-11 18:20:57 -0300

    fix autenticação usando identity

---
+++ SistemaEstoque.API/Autenticação/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaEstoque.Infra.Entidades;
+
+namespace SistemaEstoque.API.Autenticação
+{
+    public class ApplicationDbContext : IdentityDbContext<Usuario>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> modelBuilder) : base(modelBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Usuario>().ToTable("Usuario")
+                .Property(u => u.UsuarioId)
+                .HasColumnName("UsuarioId");
+
+            builder.Entity<IdentityUserRole<string>>().ToTable("UsuarioPapel");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("Logins");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("Claims");
+            builder.Entity<IdentityRole>().ToTable("Papeis");
+
+        }
+    }
+}
---
+++ SistemaEstoque.Infra/Entidades/Validações/UsuarioValidation.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace SistemaEstoque.Infra.Entidades.Validações
+{
+    public class UsuarioValidation : AbstractValidator<Usuario>
+    {
+        public UsuarioValidation()
+        {
+            RuleFor(x => x).NotEmpty().WithMessage("A entidade não pode ser vázia.")
+           .NotNull().WithMessage("Entidade não pode ser nula");
+
+            RuleFor(x => x.Nome).NotNull().WithMessage("O nome não pode ser vázio.")
+            .NotEmpty().WithMessage("O nome não pode ser nulo")
+            .MinimumLength(3).WithMessage("O nome deve ter no mínimo 3 caracteres")
+            .MaximumLength(80).WithMessage("O nome deve ter no máximo 80 caracteres");
+
+            RuleFor(x => x.PasswordHash).NotNull().WithMessage("A senha não pode ser vázia.")
+            .NotEmpty().WithMessage("A senha não pode ser nula")
+            .MinimumLength(6).WithMessage("A senha deve ter no mínimo 6 caracteres")
+            .MaximumLength(30).WithMessage("A senha deve ter no máximo 30 caracteres");
+
+            RuleFor(x => x.Email).NotNull().WithMessage("o e-mail não pode ser nulo.")
+                .NotEmpty().WithMessage("o e-mail não pode ser vázio")
+                .MinimumLength(10).WithMessage("o e-mail tem que ter no mínimo 10 caracteres")
+                .MaximumLength(180).WithMessage("o e-mail só pode ter no máximo 180 caracteres")
+            .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+            .WithMessage("e-mail não válido");
+        }
+    }
+}

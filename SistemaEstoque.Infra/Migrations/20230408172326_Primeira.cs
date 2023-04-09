031889d88e062a6fed5c0ca1d9c47af6da6cccc4
Aldo Luiz Moura <aldoluiz21@gmail.com>
2023-04-09 02:46:52 -0300

    ajustando migrations em infra project

---
+++ SistemaEstoque.Infra/Migrations/20230408172326_Primeira.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaEstoque.Infra.Migrations
+{
+    public partial class Primeira : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Documento",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Numero = table.Column<string>(type: "varchar(14)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fabricante",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(200)", nullable: false),
+                    DocumentoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fabricante", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fabricante_Documento_DocumentoId",
+                        column: x => x.DocumentoId,
+                        principalTable: "Documento",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false),
+                    DocumentoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<string>(type: "varchar(100)", nullable: true),
+                    UserName = table.Column<string>(type: "varchar(100)", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "varchar(100)", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "varchar(100)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "varchar(100)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "varchar(100)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "varchar(100)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.UsuarioId);
+                    table.ForeignKey(
+                        name: "FK_Usuario_Documento_DocumentoId",
+                        column: x => x.DocumentoId,
+                        principalTable: "Documento",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Logradouro = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Numero = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Complemento = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Cep = table.Column<string>(type: "varchar(8)", nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Estado = table.Column<string>(type: "varchar(50)", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Endereco_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "UsuarioId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Preco = table.Column<double>(type: "float", nullable: false),
+                    QuantidadeEstoque = table.Column<int>(type: "int", nullable: false),
+                    Marca = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Modelo = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(100)", nullable: true),
+                    DataVencimento = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FabricanteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoriaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produto_Categoria_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categoria",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Produto_Fabricante_FabricanteId",
+                        column: x => x.FabricanteId,
+                        principalTable: "Fabricante",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Produto_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "UsuarioId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Endereco_UsuarioId",
+                table: "Endereco",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fabricante_DocumentoId",
+                table: "Fabricante",
+                column: "DocumentoId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_CategoriaId",
+                table: "Produto",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_FabricanteId",
+                table: "Produto",
+                column: "FabricanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_UsuarioId",
+                table: "Produto",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_DocumentoId",
+                table: "Usuario",
+                column: "DocumentoId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Endereco");
+
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+
+            migrationBuilder.DropTable(
+                name: "Fabricante");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Documento");
+        }
+    }
+}

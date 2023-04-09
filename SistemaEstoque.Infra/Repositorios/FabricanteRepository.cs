ef36ad8562fd1c6b8ef3768b6b424cdd04d7ae5b
Aldo Luiz Moura <aldoluiz21@gmail.com>
2023-04-09 02:47:04 -0300

    add fabricanteRepository

---
+++ SistemaEstoque.Infra/Repositorios/FabricanteRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaEstoque.Infra.Contexto;
+using SistemaEstoque.Infra.Entidades;
+using SistemaEstoque.Infra.Interfaces.Repositorio;
+
+namespace SistemaEstoque.Infra.Repositorios
+{
+    public class FabricanteRepository : Repository<Fabricante>, IFabricanteRepository
+    {
+        public FabricanteRepository(DbContextEstoque contexto) : base(contexto) { }
+        public async Task<IEnumerable<Fabricante>> ObterFabricantes() => await Db.Fabricantes.AsNoTracking().ToListAsync();
+        public async Task<Fabricante> ObterPorNome(string nomeFabricante) => await Db.Fabricantes.AsNoTracking().Where(c => c.Nome == nomeFabricante).FirstOrDefaultAsync();
+    }
+}

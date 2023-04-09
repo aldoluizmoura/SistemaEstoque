f9228aaa7e1f518c02a9cc9be59d2738585d9d49
Aldo Luiz Moura <aldoluiz21@gmail.com>
2023-04-09 02:45:28 -0300

    add novas conf no program.cs

--- SistemaEstoque.API/Program.cs
+++ SistemaEstoque.API/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SistemaEstoque.API.Autenticação;
 using SistemaEstoque.API.AutoMapper;
+using SistemaEstoque.API.Models;
 using SistemaEstoque.Infra.Contexto;
 using SistemaEstoque.Infra.Entidades;
 using SistemaEstoque.Infra.Interfaces.Repositorio;
@@ -9,6 +13,7 @@ using SistemaEstoque.Infra.Repositorios;
 using SistemaEstoque.Negocio;
 using SistemaEstoque.Negocio.Interfaces;
 using SistemaEstoque.Negocio.Notificacões;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,11 +35,42 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //Identity config
 builder.Services.AddIdentity<Usuario, IdentityRole>()
     .AddRoles<IdentityRole>()    
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 //outras config
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Serviço de Estoque", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme." +
+        "\r\n\r\n Enter 'Bearer'[space] and then your token in the text input below." +
+        "\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
+
 builder.Services.AddAutoMapper(typeof(InfraToApiMappingProfile),
                                typeof(ApiToInfraMappingProfile));
 
@@ -53,7 +89,33 @@ builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
 builder.Services.AddScoped<INotificador, Notificador>();
 
+//JWT
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettingsSection);
 
+var appSettings = appSettingsSection.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = true;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = appSettings.ValidoEm,
+        ValidIssuer = appSettings.Emissor
+    };
+});
+
+//app Configurations
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

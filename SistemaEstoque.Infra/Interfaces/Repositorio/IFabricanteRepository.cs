6b0dd0471c1a488149d28a888016bcfa75377624
Aldo Luiz Moura <aldoluiz21@gmail.com>
2023-04-09 02:46:37 -0300

    add ifabricanteRepository

---
+++ SistemaEstoque.Infra/Interfaces/Repositorio/IFabricanteRepository.cs
@@ -0,0 +1,10 @@
+﻿using SistemaEstoque.Infra.Entidades;
+
+namespace SistemaEstoque.Infra.Interfaces.Repositorio
+{
+    public interface IFabricanteRepository : IRepository<Fabricante>
+    {
+        Task<IEnumerable<Fabricante>> ObterFabricantes();
+        Task<Fabricante> ObterPorNome(string nomeFabricante);
+    }
+}
